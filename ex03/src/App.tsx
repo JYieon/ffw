0c8d2b8b3adf81880f7320c6b79b477c0e9d4251
choijy <dkcl.8642@gmail.com>
2024-04-22 23:45:01 +0900

    입력폼

--- ex03/src/App.tsx
+++ ex03/src/App.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  let count = 0;
-  const increase = () => { ++count; console.log(count); };
-  const decrease = () => { --count; console.log(count); };
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const errorname = <p>아이디를 입력하세요.</p>
+  const errorage = <p>비밀번호를 입력하세요.</p>
   return (
-    <div>
-      <p>{ count }</p>
-      <button onClick={increase}>증가</button>
-      <button onClick={decrease}>감소</button>
+    <div id="app">
+      <h1>입력폼</h1>
+      <div className="control">
+        <input type="text" placeholder="아이디"
+               onChange={(e) => setName(e.target.value)} value={name} />
+        {name ? null : errorname}
+      </div>
+      <div className="control">
+        <input type="password" placeholder="비밀번호"
+               onChange={(e) => setAge(e.target.value)} value={age} />
+        {age ? null : errorage}       
+      </div>
+      <hr />
+      <h1>입력된 값</h1>
+      <div className="control">아이디: {name}</div>
+      <div className="control">비밀번호: {age}</div>
     </div>
   );
 }
